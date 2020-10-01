071216b6b4258ac3ed46e675716046339355e0b2
Ferenc Hammerl <hammerl.ferenc@gmail.com>
2020-10-01 20:45:02 +0100

    Add spaces echo statements to dodge line ending issue
    Zipping changes the line endings to CRLF, which in turn confuses command line parameters.
    
    Figure out a .gitattributes solution (*.txt eol=crlf)

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -2,7 +2,7 @@ sleep 0.2 # So we log under db_1 :)
 echo "Hacky build-script is starting up. We're reusing the devcontainer (as opposed to getting a proper JRE image)"
 echo "This is going to take long the first time, Maven needs to install lots of dependencies"
 cd workspace 
-/usr/local/sdkman/candidates/maven/current/bin/mvn -T 4 package -DskipTests -nsu -ntp
-cd target
+/usr/local/sdkman/candidates/maven/current/bin/mvn -T 4 package -DskipTests -nsu -ntp && echo "Done"
+cd target 
 echo "Running spring application"
-java -jar polls-0.0.1-SNAPSHOT.jar
+java -jar polls-0.0.1-SNAPSHOT.jar && echo "Done"
