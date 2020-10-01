93658d1d48cf9c0bf37a36510b554c04383132c8
Ferenc Hammerl <hammerl.ferenc@gmail.com>
2020-10-01 16:55:15 +0100

    Add a file to insert dummy data
    Will be useful for integration tests later, too.

---
+++ dbconfig/insert_rows.sql
@@ -0,0 +1,28 @@
+INSERT INTO poll(
+	id, latest_change, initiated, hidden, preferences_type, state, title)
+	VALUES ('xsd4cv89t5f5um4b', null, now(), false, 'YESNO', 'OPEN', 'Qui sont les superhéros Marvel les plus oufs?');
+
+INSERT INTO participant(
+	name, email, poll_id)
+	VALUES ('Jane Doe', 'mh+sample@doodle.com', 'xsd4cv89t5f5um4b');
+    
+INSERT INTO participant(
+	name, email, poll_id)
+	VALUES ('George Doe', 'mh+sample@doodle.com', 'xsd4cv89t5f5um4b');
+
+INSERT INTO participant(
+	name, email, poll_id)
+	VALUES ('Markus Doe', 'mh+sample@doodle.com', 'xsd4cv89t5f5um4b');
+
+
+INSERT INTO poll(
+	id, latest_change, initiated, hidden, preferences_type, state, title)
+	VALUES ('v89t5f5um4bxsd4c', null, now(), false, 'YESNO', 'OPEN', '谁是最坏蛋奇迹超级英雄?');
+
+INSERT INTO participant(
+	name, email, poll_id)
+	VALUES ('Revi Doe', 'mh+sample@doodle.com', 'v89t5f5um4bxsd4c');
+    
+INSERT INTO participant(
+	name, email, poll_id)
+	VALUES ('Johannes Doe', 'mh+sample@doodle.com', 'v89t5f5um4bxsd4c');
