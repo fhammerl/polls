9b9e07d860e7469b11a4aac98e6a418bd0f3457b
Ferenc Hammerl <hammerl.ferenc@gmail.com>
2020-10-01 16:54:19 +0100

    Create poll and participant tables

---
+++ dbconfig/create_tables.sql
@@ -0,0 +1,25 @@
+CREATE TABLE poll
+(
+    id CHAR(16) NOT NULL,
+    latest_change timestamp without time zone,
+    initiated timestamp without time zone NOT NULL,
+    hidden boolean NOT NULL,
+    preferences_type text NOT NULL,
+    state text NOT NULL,
+    title text NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE participant
+(
+    id SERIAL,
+    name text NOT NULL,
+    email text NOT NULL,
+    poll_id character(16) NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_participant_poll FOREIGN KEY (poll_id)
+        REFERENCES public.poll (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+)
